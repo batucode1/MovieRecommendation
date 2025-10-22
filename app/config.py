520118318478bee832a06a3b785cb1efc5a037b5
batucode1 <batuhanokul52@gmail.com>
2025-10-22 23:03:05 +0300

    config

--- app/config.py
+++ app/config.py
@@ -6,11 +6,11 @@ from langchain_huggingface import HuggingFaceEmbeddings
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
 os.environ["ANONYMIZED_TELEMETRY"] = "False"
 os.environ["CHROMA_TELEMETRY_DISABLED"] = "true"
-
 load_dotenv()
-
 model = ChatOpenAI(model="gpt-3.5-turbo", temperature=0.1)
-
-movies_file = "data/movies.csv"
+movies_file = "../data/movies.csv"
 embedding_model = "sentence-transformers/paraphrase-multilingual-mpnet-base-v2"
-embedding = HuggingFaceEmbeddings(model_name=embedding_model)
+embedding = HuggingFaceEmbeddings(
+    model_name=embedding_model)
+
+# denemek için daha küçük model -> sentence-transformers/all-MiniLM-L6-v2
